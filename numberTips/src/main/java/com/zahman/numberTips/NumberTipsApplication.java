1bc144ee95a353574f003ffd8e9dd49655632039
Martin Zahradil <martin.zahradil@isonsoft.cz>
2019-08-03 22:53:32 +0200

    project intial

---
+++ numberTips/src/main/java/com/zahman/numberTips/NumberTipsApplication.java
@@ -0,0 +1,13 @@
+package com.zahman.numberTips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberTipsApplication.class, args);
+	}
+
+}
---
+++ numberTips/src/test/java/com/zahman/numberTips/NumberTipsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zahman.numberTips;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NumberTipsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
